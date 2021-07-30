aed8b78cb2a9d542d1a9cf44c6a3fbfb114209bf
Philippe Geraldeli <phgeraldeli@id.uff.br>
2021-07-30 00:48:22 -0300

    Criando eks usando modulo da documentação oficial

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -1,15 +1,41 @@
-resource "aws_eks_cluster" "tf_eks" {
-  name            = "eks-cluster"
-  role_arn        = "${aws_iam_role.eks-master.arn}"
-  version	      = "${var.cluster_version}"
-  vpc_config {
-    security_group_ids = ["${aws_security_group.eks-master.id}"]
-    subnet_ids = flatten(["${var.app_subnet_ids}"])
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.20"
+  subnets         = flatten(["${var.app_subnet_ids}"])
 
+  tags = {
+    Name = "eks-cluster"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
 }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.tf-cluster-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.tf-cluster-AmazonEKSServicePolicy,
-  ]
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -2,23 +2,10 @@ variable "vpc_id" {
   type = string
 }
 
-variable "accessing_computer_ip" {
-    type = string
-    description = "Public IP of the computer accessing the cluster via kubectl or browser."
-}
-
 variable "aws_region" {
-  type = string
-}
-
-variable "keypair-name" {
   type = string
 }
 
 variable "app_subnet_ids" {
   type = list
-}
-
-variable "cluster_version" {
-  type = string
 }
